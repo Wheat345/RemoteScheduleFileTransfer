1c8642bd0759832fd60bab81bfbff80032dd74ec
Wheat345 <Feng_di@hotmail.com>
2020-03-08 17:11:25 -0400

    Update scp.sh

--- src/main/resources/bin/scp.sh
+++ src/main/resources/bin/scp.sh
@@ -4,4 +4,4 @@ sourcePath=$2
 destinationHost=$3 #TODO: use this varible in remote destination.
 destinationPath=$4
 mkdir $destinationPath
-sshpass -p "password" scp -r dif@$sourceHost:$sourcePath $destinationPath
+sshpass -p "password" scp -r user@$sourceHost:$sourcePath $destinationPath
